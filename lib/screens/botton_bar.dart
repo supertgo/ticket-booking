586fe320280add7f4784255eff71e6871b3b3e8a
supertgo <thiagoroberto2003@gmail.com>
2022-10-30 19:36:51 -0300

    feat(main): add hotel

--- lib/screens/botton_bar.dart
+++ lib/screens/botton_bar.dart
@@ -2,6 +2,7 @@ import 'package:fluentui_icons/fluentui_icons.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:ticket_booking/screens/home_screen.dart';
 
 class BottomBar extends StatefulWidget {
   const BottomBar({super.key});
@@ -13,7 +14,7 @@ class BottomBar extends StatefulWidget {
 class _BottomBarState extends State<BottomBar> {
   int _selectedIndex = 0;
   static final List<Widget> _widgetOptions = <Widget>[
-    const Text('Home'),
+    HomeScreen(),
     const Text('Search'),
     const Text('Tickets'),
     const Text('Profile'),
@@ -28,9 +29,6 @@ class _BottomBarState extends State<BottomBar> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('My tickets'),
-      ),
       body: Center(child: _widgetOptions[_selectedIndex]),
       bottomNavigationBar: BottomNavigationBar(
         elevation: 10,
---
+++ lib/screens/hotel_screen.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
+import 'package:ticket_booking/utils/app_layout.dart';
+import 'package:ticket_booking/utils/app_styles.dart';
+
+class HotelScreen extends StatelessWidget {
+  const HotelScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final size = AppLayout.getSize(context);
+    return Container(
+        width: size.width * 0.6,
+        height: 350,
+        padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 17),
+        margin: const EdgeInsets.only(
+          right: 17,
+          top: 5,
+        ),
+        decoration: BoxDecoration(
+          color: Styles.primaryColor,
+          borderRadius: BorderRadius.circular(24),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey.shade200,
+              blurRadius: 2,
+              spreadRadius: 1,
+            )
+          ],
+        ),
+        child: Column(children: [
+          Container(
+            height: 180,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(12),
+              color: Styles.primaryColor,
+              image: const DecorationImage(
+                fit: BoxFit.cover,
+                image: AssetImage("assets/images/one.png"),
+              ),
+            ),
+          ),
+          const Gap(10),
+          Text(
+            "Open Space",
+            style: Styles.headLineStyle2.copyWith(color: Styles.kakiColor),
+          ),
+          const Gap(10),
+          Text(
+            "London",
+            style: Styles.headLineStyle3.copyWith(color: Colors.white),
+          ),
+          const Gap(10),
+          Text(
+            "\$40/night",
+            style: Styles.headLineStyle3.copyWith(color: Styles.kakiColor),
+          ),
+        ]));
+  }
+}

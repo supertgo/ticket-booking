d22fc16366b0c0b2e27faea80ce515b98f7d7c08
supertgo <thiagoroberto2003@gmail.com>
2022-12-11 14:00:06 -0300

    feat(ticket): add reusable widgets to handle with ticket screen

---
+++ lib/screens/ticket_screen.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:gap/gap.dart';
+import 'package:ticket_booking/screens/ticket_view.dart';
+import 'package:ticket_booking/utils/app_info_list.dart';
+import 'package:ticket_booking/utils/app_layout.dart';
+import 'package:ticket_booking/utils/app_styles.dart';
+import 'package:ticket_booking/widgets/column_layout.dart';
+import 'package:ticket_booking/widgets/ticket_tabs.dart';
+
+class TicketScreen extends StatelessWidget {
+  const TicketScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final size = AppLayout.getSize(context);
+    return Scaffold(
+      backgroundColor: Styles.bgColor,
+      body: Stack(
+        children: [
+          ListView(
+            padding: EdgeInsets.symmetric(
+              horizontal: AppLayout.getHeight(20),
+              vertical: AppLayout.getHeight(20),
+            ),
+            children: [
+              Gap(AppLayout.getHeight(40)),
+              Text('Tickets', style: Styles.headLineStyle),
+              Gap(AppLayout.getHeight(20)),
+              const AppTicketTabs(firstTab: 'Upcoming', secondTab: 'Previous'),
+              Gap(AppLayout.getHeight(20)),
+              Container(
+                padding: EdgeInsets.only(
+                  left: AppLayout.getHeight(15),
+                ),
+                child: TicketView(
+                  ticket: ticketList[0],
+                  isColor: true,
+                ),
+              ),
+              Container(
+                color: Colors.white,
+                padding:
+                    EdgeInsets.symmetric(horizontal: AppLayout.getHeight(15)),
+                margin:
+                    EdgeInsets.symmetric(horizontal: AppLayout.getHeight(15)),
+                child: Column(
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: const [
+                        AppColumnLayout(
+                          firstText: 'Flutter Db',
+                          secondText: 'Passenger',
+                          alignment: CrossAxisAlignment.start,
+                          isColor: false,
+                        ),
+                        AppColumnLayout(
+                          firstText: '5221 478566',
+                          secondText: 'Passport',
+                          alignment: CrossAxisAlignment.end,
+                          isColor: false,
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
